96e5a5a72d8468ee6ebf82a467f9081d26b473b3
Isabela Fernandes Guerra de Morais <bebelicha@iMac-de-Isabela.local>
2021-07-12 17:28:42 -0300

    Fim primeira aula

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,33 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { AlurakutStyles } from '../src/lib/AlurakutCommons';
 
 const GlobalStyle = createGlobalStyle`
-  body {
+  /* Reset CSS (Necolas Reset CSS <3) */
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
   }
+  body {
+    font-family: sans-serif;
+    background-image: url("https://live.staticflickr.com/2924/14610038970_04e0b11b05_c.jpg");
+  }
+  #__next {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+  }
+  ${AlurakutStyles}
 `
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: 'red',
   },
 }
 
